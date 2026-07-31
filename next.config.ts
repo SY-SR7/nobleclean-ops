d7ab54d45c6297304a0c93f875439d71a64213b5
SY-SR7 <ammarsy79@gmail.com>
2026-07-31 16:58:49 +0200

    auto-sync: 2026-07-31 16:58:49

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
-const serverActionBodySizeLimit = "1mb";
+const serverActionBodySizeLimit = "4mb";
 
 const scriptSource = isDevelopment
   ? "script-src 'self' 'unsafe-inline' 'unsafe-eval'"
